27392beabd8cba666f2c20ec1c066334af1d4b4c
Braan7 <braanx7@gmail.com>
2025-05-22 19:14:03 -0600

    Actualizar group-precios2.js

--- plugins/group-precios2.js
+++ plugins/group-precios2.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn }) => {
 
     if (m.isGroup) {
         // URL de la imagen
-        const imageUrl = 'https://qu.ax/vBjJa.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
+        const imageUrl = 'https://qu.ax/OgXMA.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
 
         // Envía la imagen con el mensaje
         await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
